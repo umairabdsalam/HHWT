44081311a736b2af499dac72e5061b8c9da4313e
Umair Abdus Salam <umairabdsalam@gmail.com>
2024-06-17 22:09:39 +0100

    Initial Commit

---
+++ HHWT/HHWTApp.swift
@@ -0,0 +1,17 @@
+//
+//  HHWTApp.swift
+//  HHWT
+//
+//  Created by Umair Salam on 6/17/24.
+//
+
+import SwiftUI
+
+@main
+struct HHWTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
